48f96770c1e233a063030e4a6ef5f1275251dde9
Diwashd <ragnarwho@gmail.com>
2023-10-04 12:09:14 +0545

    left side bar implemented

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -23,16 +23,15 @@ const NavContent = () => {
           (pathname.includes(item.route) && item.route.length > 1) ||
           pathname === item.route;
         return (
-          <SheetClose
-            asChild
-            key={item.route}
-            className={`${
-              isActive
-                ? "primary-gradient rounded-lg text-light-900 "
-                : "text-dark300_light900"
-            } flex items-center justify-start gap-4 bg-transparent p-4`}
-          >
-            <Link href={item.route} className="">
+          <SheetClose asChild key={item.route}>
+            <Link
+              href={item.route}
+              className={`${
+                isActive
+                  ? "primary-gradient rounded-lg text-light-900 "
+                  : "text-dark300_light900"
+              } flex items-center justify-start gap-4 bg-transparent p-4`}
+            >
               <Image
                 src={item.imgURL}
                 alt={item.label}
